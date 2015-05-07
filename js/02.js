9ff89a0f4a036e889b97f881a4a6c753df6a7a63
Akari-n <s14560at@sfc.keio.ac.jp>
2015-05-07 11:39:30 +0900

    練習問題ときました

---
+++ js/02.js
@@ -0,0 +1,13 @@
+function zero(){
+  return [];
+}
+
+/*　上と同じ意味になる
+　
+function zero(){
+  var a = [];
+
+  return a;
+}
+
+*/
---
+++ js/03.js
@@ -0,0 +1,9 @@
+function succ(a){
+  return a;
+}
+
+
+function succ(a){
+  a.push(true);
+  return a;
+}
